820146eaf494473ce1e13e75919411d1855243f2
David Payne <david.payne@dominos.com.au>
2025-11-28 16:26:50 +0800

    fix(playwright): add timeout configuration for expect assertions

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [['html'], ['list']],
+  expect: {
+    timeout: 10 * 1000,
+  },
   use: {
     baseURL: baseURL,
     trace: 'on-first-retry',
